d8cd14a10c433a708a5369e66cb7d6270cf5b220
Mahad Ali <mahadaali3@gmail.com>
2023-06-27 02:39:51 -0600

    first commit

---
+++ java-proj-4/starter/src/main/java/com/devmountain/noteApp/services/UserService.java
@@ -0,0 +1,12 @@
+package com.devmountain.noteApp.services;
+
+import com.devmountain.noteApp.dtos.UserDto;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+public interface UserService {
+    @Transactional
+    List<String> addUser(UserDto userDto);
+    List<String> userLogin(UserDto userDto);
+}
